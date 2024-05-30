a9667f25b19607d719ab2553dc78f16b60ca4b5d
김명구 <myeonggu.kim@kakao.com>
2024-05-30 17:01:50 +0900

    datasource 멀티 구성 예시 추가

---
+++ approval-consumer/src/main/java/lxhausys/mybatis/api/datasource/MssqlMapper.java
@@ -0,0 +1,23 @@
+package lxhausys.mybatis.api.datasource;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import lxhausys.mybatis.api.employ.dto.EmployPageREQ;
+import lxhausys.mybatis.api.employ.dto.EmployVO;
+import lxhausys.mybatis.config.mybatis.Pageable;
+import lxhausys.mybatis.config.mybatis.PagedList;
+
+@Mapper
+public interface MssqlMapper {
+  
+  List<EmployVO> selectAll();
+  PagedList<EmployVO> selectList(Pageable param);
+  PagedList<EmployVO> selectListByName(EmployPageREQ param);
+  int insert(EmployVO param);
+  EmployVO select(String id);
+  int update(EmployVO param);
+  int delete(EmployVO param);
+  
+}
